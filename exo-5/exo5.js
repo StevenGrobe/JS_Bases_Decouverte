2b2324595a9090230b3bbf981f66104c9b5808f8
StevenGrobe <steven.grobe@le-campus-numerique.fr>
2021-02-17 15:24:58 +0100

    Exo-5 PPV

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,16 @@
 // you can write js here
-
 console.log('exo-5');
+
+// let input = "she sells sea shells by the sea shore".toLowerCase();
+let input = prompt("Entrez votre phrase à traduire ici").toLowerCase();
+const vowels = ['a','e','i','o','u','y'];
+let resultArray = [];
+
+for (i = 0; i < input.length; i++) { // boucle for pour lire mon input
+    for (j = 0; j < vowels.length; j++) { // boucle for pour lire le tableau vowels
+        if (input[i] === vowels[j]) { // Si une valeur de input = à vowels
+            resultArray.push(input[i]); // Remplis le tableau avec la valeur de l'input
+            }
+        }
+}
+console.log(resultArray.join()); // Methode join() renvoye une chaine de charactere qui correspond aux entrées de notre array
