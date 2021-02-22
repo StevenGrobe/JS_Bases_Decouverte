b0c294839dfd61f97c03e470af59f06a8514498e
StevenGrobe <grobesteven@gmail.com>
2021-02-22 13:17:05 +0100

    exo-5 Validé

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,16 +1,16 @@
 // you can write js here
 console.log('exo-5');
 
-// let input = "she sells sea shells by the sea shore".toLowerCase();
-let input = prompt("Entrez votre phrase à traduire ici").toLowerCase();
+const input = "she sells sea shells by the sea shore".toLowerCase();
+/* const input = prompt("Entrez votre phrase à traduire ici"); */
 const vowels = ['a','e','i','o','u','y'];
 let resultArray = [];
 
-for (i = 0; i < input.length; i++) { // boucle for pour lire mon input
-    for (j = 0; j < vowels.length; j++) { // boucle for pour lire le tableau vowels
+for (let i = 0; i < input.length; i++) { // boucle for pour lire mon input
+    for (let j = 0; j < vowels.length; j++) { // boucle for pour lire le tableau vowels
         if (input[i] === vowels[j]) { // Si une valeur de input = à vowels
             resultArray.push(input[i]); // Remplis le tableau avec la valeur de l'input
             }
         }
 }
-console.log(resultArray.join()); // Methode join() renvoye une chaine de charactere qui correspond aux entrées de notre array
+console.log(resultArray.join('').toUpperCase()); // Methode join() renvoye une chaine de charactere qui correspond aux entrées de notre array
