05f8073a363799c8688c63105a0899536cd2b453
StevenGrobe <grobesteven@gmail.com>
2021-02-22 13:16:59 +0100

    exo-4 Validé

--- exo-4/script.js
+++ exo-4/script.js
@@ -6,7 +6,7 @@ const secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "righ
 const poppedSecretMessage = secretMessage.pop(); // Enlève la dernière valeur du tableau
 const pushedSecretMessage = secretMessage.push('to', 'program'); // Rajoute des entrées à mon array
 const shiftedSecretMessage = secretMessage.shift(); // Delete la premiere entrée de l'array
-const EasilySecretMessage = secretMessage[secretMessage.indexOf('right')] = "easily" // Remplace l'entrée 5 de mon array par "easily"
+const EasilySecretMessage = secretMessage[secretMessage.indexOf('right')] = "easily" // Remplace l'entrée 'right' de mon array par "easily"
 const unshiftedSecretMessage = secretMessage.unshift('Programming') // Ajoute la première entrée de l'array
-const splicedSecretMessage = secretMessage.splice(secretMessage.indexOf(5), 5, 'know'); // Remplace l'entrée 5 de l'array, 5 valeurs, 'know'
-console.log(secretMessage.join()); // Methode join() renvoye une chaine de charactere qui correspond aux entrées de notre array
+const splicedSecretMessage = secretMessage.splice(5, 5, 'know'); // Remplace l'entrée 5 de l'array, 5 valeurs, 'know'
+console.log(secretMessage.join(' ')); // Methode join() renvoye une chaine de charactere qui correspond aux entrées de notre array
